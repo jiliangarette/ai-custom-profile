f86a0052803ecc904fb1fc563edd4148e8747f1f
Jgarette0 <aajiliangarette@gmail.com>
2025-01-15 00:02:47 +0800

    setup tailwind css and add gemini ai dependency

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold text-green-400 underline">Hello world!</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  css: ["~/assets/styles/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  runtimeConfig: {
+    // server
+    geminiApiKey: process.env.VITE_GEMINI_API_KEY || "",
+
+    // client
+    public: {
+      geminiApiKey: process.env.VITE_GEMINI_API_KEY || "",
+    },
+  },
+
+  modules: ["nuxt-lucide-icons"],
+});
