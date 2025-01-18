7c58d513f7585acc1d8742fa83d757fa8e19f4eb
Jilian Garette A. Abangan <92016876+jiliangarette@users.noreply.github.com>
2025-01-18 18:53:08 +0800

    Modified the type safe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ const Home = () => {
         const data = await response.json();
         setAgentsData(data?.data || "No data available");
         setAgentsName(data?.user || "Agent");
-      } catch (error : string) {
+      } catch (error : unknown) {
         setError(error.message);
       }
     };
