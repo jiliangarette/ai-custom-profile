6a2e55ecfc2d1997b1212551ccfd7e77778f4f64
Jgarette0 <aajiliangarette@gmail.com>
2025-01-23 02:09:44 +0800

    enhance ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { BannerThreeDCard } from "@/components/BannerThreeDCard";
+import { ActionThreeDCard } from "@/components/ActionThreeDCard";
 
 interface Profile {
   id: string;
@@ -43,78 +45,131 @@ export default function LandingPage() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col">
-      <div className="sm:h-screen bg-[#254f1a] w-screen flex sm:flex-row flex-col py-40 justify-center">
-        <div className="flex flex-col justify-center w-full h-full gap-8 px-4 sm:px-8 lg:px-16">
-          <div className="space-y-6   w-full">
-            <h1
-              className=" font-extrabold  leading-none tracking-tight text-[#d2e823]"
-              style={{
-                fontSize: "clamp(32px, 11.5vmin, 88px)",
-              }}>
-              Everything you are. In one, simple link in bio.
-            </h1>
-            <p className=" text-[#d2e823] font-semibold md:text-xl">
-              Join 50M+ people using Linktree for their link in bio. One link to
-              help you share everything you create, curate and sell from your
-              Instagram, TikTok, Twitter, YouTube and other social media
-              profiles.
-            </p>
-          </div>
-          <div className="flex flex-col gap-4 sm:flex-row">
-            <form
-              onSubmit={handleSubmit}
-              className="flex w-full  items-center flex-wrap space-x-2">
-              <div className="relative max-w-[243px]   my-1">
-                <div className="absolute inset-y-0 left-3 text-md font-semibold px-2 flex items-center pointer-events-none text-muted-foreground">
-                  aiprofile.com/
-                </div>
-                <Input
-                  type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  className=" p-8 px-10 pl-[122px] bg-white border-none min-w-[243px] h-[62px] text-md font-semibold text-slate-800"
-                  placeholder="yourname"
-                />
-              </div>
-              <Button
-                type="submit"
-                className="h-[62px] min-w-[212px] text-md rounded-full font-semibold bg-[#e9c0e9] text-slate-800"
-                size="default">
-                Claim your AIprofile
-              </Button>
-            </form>
-          </div>
-        </div>
-        <div className="w-full h-full">image</div>
-      </div>
-      <div className="relative flex-wrap flex gap-2 flex-row">
-        {profiles.map((profile) => (
-          <Link
-            key={profile.id}
-            href={`/${profile.username.replace(/\s+/g, "-").toLowerCase()}`}>
-            <Card className="flex flex-col items-center space-y-4 p-6">
-              <div className="relative h-24 w-24">
-                <div className="absolute -inset-0.5 animate-tilt rounded-full bg-gradient-to-r from-slate-500 to-slate-700" />
-                <div className="relative h-full w-full overflow-hidden rounded-full border-2 border-white bg-slate-50">
-                  <Image
-                    src={
-                      profile.image ||
-                      "https://i.pinimg.com/736x/9c/0b/44/9c0b4442c5eb323aa042644041c96414.jpg"
-                    }
-                    alt="Avatar"
-                    width={96}
-                    height={96}
-                    className="h-full w-full object-cover"
+    <div className="flex min-h-screen flex-col overflow-hidden">
+      <div className="bg-[#254f1a]  flex justify-center w-full">
+        <div className="sm:h-screen flex sm:flex-row flex-col pt-40 sm:py-40 justify-center w-full  max-w-[1300px]">
+          <div className="flex flex-col justify-center w-full  h-full gap-8 px-4 ">
+            <div className="space-y-6   w-full">
+              <h1
+                className=" font-extrabold  leading-none tracking-tight text-[#d2e823]"
+                style={{
+                  fontSize: "clamp(32px, 10.5vmin, 88px)",
+                }}>
+                Everything you are. In one, simple link in bio.
+              </h1>
+              <p className=" text-[#d2e823] font-semibold md:text-xl">
+                Join people using aiprofile for their link in bio. One link to
+                help you share everything you create, curate and sell from your
+                Instagram, TikTok, Twitter, YouTube and other social media
+                profiles.
+              </p>
+            </div>
+            <div className="flex flex-col gap-4 sm:flex-">
+              <form
+                onSubmit={handleSubmit}
+                className="flex w-full flex-row gap-x-4 flex-wrap ">
+                <div className="relative max-w-[243px] my-1">
+                  <div className="absolute inset-y-0 left-3 text-md font-semibold px-2 flex items-center pointer-events-none text-muted-foreground">
+                    aiprofile.com/
+                  </div>
+                  <Input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="  pl-[122px] bg-white border-none min-w-[243px] sm:min-w-[254px] h-[62px] text-md font-semibold text-slate-800"
+                    maxLength={15}
+                    placeholder="yourname"
                   />
                 </div>
-              </div>
-              <div className="text-center">
-                <h2 className="text-xl font-semibold">/{profile.username}</h2>
-              </div>
-            </Card>
-          </Link>
-        ))}
+                <Button
+                  type="submit"
+                  className="h-[62px] min-w-[212px] text-md rounded-full font-semibold bg-[#e9c0e9] text-slate-800"
+                  size="default">
+                  Claim your AIprofile
+                </Button>
+              </form>
+            </div>
+          </div>
+          <div className="w-full sm:h-full flex flex-col  justify-center place-items-center">
+            <div className="w-[545px]  h-[545px]  flex justify-center place-items-center ">
+              <BannerThreeDCard />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#e9c0e9]  flex justify-center w-full">
+        <div className="sm:h-screen flex sm:flex-row-reverse flex-col py-28 justify-center w-full  max-w-[1300px]">
+          <div className="flex flex-col justify-center w-full  h-full gap-8 px-4 ">
+            <div className="space-y-3   w-full">
+              <h1
+                className=" font-extrabold  leading-none tracking-tight text-[#502274]"
+                style={{
+                  fontSize: "clamp(32px, 10.5vmin, 88px)",
+                }}>
+                Create and customize your Linktree in minutes
+              </h1>
+              <p className=" text-[#502274] font-medium md:text-xl">
+                Connect your TikTok, Instagram, Twitter, website, store, videos,
+                music, podcast, events and more. It all comes together in a link
+                in bio landing page designed to convert.
+              </p>
+            </div>
+            <div className="flex flex-col gap-4 sm:flex-row">
+              <form
+                onSubmit={handleSubmit}
+                className="flex w-full flex-row gap-x-4 flex-wrap ">
+                <Button
+                  type="submit"
+                  className="h-[62px] min-w-[212px]  text-md rounded-full font-semibold bg-[#502274] text-slate-100"
+                  size="default">
+                  Get started for free
+                </Button>
+              </form>
+            </div>
+          </div>
+          <div className="w-full h-full flex py-20 justify-center place-items-center">
+            <div className="w-[545px]  h-[545px]  flex justify-center place-items-center ">
+              <ActionThreeDCard />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#50be5033]  flex justify-center w-full">
+        <div className="sm:h-screen flex sm:flex-row flex-col py-40 justify-center w-full  max-w-[1300px]">
+          <div className="relative flex-wrap flex gap-2 flex-row">
+            {profiles.map((profile) => (
+              <Link
+                key={profile.id}
+                href={`/${profile.username
+                  .replace(/\s+/g, "-")
+                  .toLowerCase()}`}>
+                <Card className="flex flex-col items-center space-y-4 p-6">
+                  <div className="relative h-24 w-24">
+                    <div className="absolute -inset-0.5 animate-tilt rounded-full bg-gradient-to-r from-slate-500 to-slate-700" />
+                    <div className="relative h-full w-full overflow-hidden rounded-full border-2 border-white bg-slate-50">
+                      <Image
+                        src={
+                          profile.image ||
+                          "https://i.pinimg.com/736x/9c/0b/44/9c0b4442c5eb323aa042644041c96414.jpg"
+                        }
+                        alt="Avatar"
+                        width={96}
+                        height={96}
+                        className="h-full w-full object-cover"
+                      />
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <h2 className="text-xl font-semibold">
+                      /{profile.username}
+                    </h2>
+                  </div>
+                </Card>
+              </Link>
+            ))}
+          </div>
+          <div>wow</div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ActionThreeDCard.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "./ui/cool-card";
+import { Facebook, Github, Instagram } from "lucide-react";
+
+export function ActionThreeDCard() {
+  return (
+    <CardContainer className="inter-var w-full  min-w-80 ">
+      <CardBody className="relative group/card w-full sm:w-[33rem] flex h-auto rounded-xl    ">
+        <div className="absolute right-0 sm:right-4 top-20">
+          <CardItem translateZ="25" className="w-full flex justify-center ">
+            <Image
+              src="https://i.pinimg.com/736x/d7/3d/8f/d73d8f2884276e5cf356653d505ea3dd.jpg"
+              alt="image"
+              className=" overflow-hidden w-[100px] h-[100px] md:w-[150px] md:h-[150px] rounded-full"
+              width={100}
+              height={100}
+            />
+          </CardItem>
+        </div>
+        <CardItem translateZ="50" className="w-full flex justify-center ">
+          <Image
+            src="https://i.pinimg.com/736x/f5/5b/2c/f55b2c1bbbaaad3072ca9c5a580bd8d0.jpg"
+            alt="image"
+            className="rounded-lg overflow-hidden  w-[160px] h-[375px] md:w-[252px] md:h-[545px]"
+            width={252}
+            height={545}
+          />
+        </CardItem>
+        <div className="absolute left-0  bottom-12">
+          <CardItem translateZ="75" className="w-full flex justify-center ">
+            <Image
+              src="https://i.pinimg.com/736x/21/39/81/2139816f87347258818d727024407802.jpg"
+              alt="image"
+              className="rounded-lg overflow-hidden w-[120px] h-[160px] md:w-[185px] md:h-[220px]"
+              width={150}
+              height={200}
+            />
+          </CardItem>
+        </div>
+
+        <div className="absolute right-0 sm:right-8 bottom-20 sm:bottom-24">
+          <CardItem
+            translateZ="75"
+            className="w-full flex justify-center  gap-1 sm:pr-8">
+            <div className="bg-[#0744c2]  p-2 rounded-full text-slate-200">
+              <Instagram size={20} />
+            </div>
+            <div className="bg-[#0744c2] p-2 rounded-full text-slate-200">
+              <Facebook size={20} />
+            </div>
+            <div className="bg-[#0744c2]  p-2 rounded-full text-slate-200">
+              <Github size={20} />
+            </div>
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
+}
